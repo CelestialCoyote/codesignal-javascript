00c69c6cd1235c600fff8052418578b7203f8f97
Paul Stearns <celestialcoyote@gmail.com>
2023-03-14 22:04:48 -0500

    started areSimilar

--- arcade04-exploring-the-waters.js
+++ arcade04-exploring-the-waters.js
@@ -53,4 +53,17 @@ function addBorder(picture) {
 	return picture;
 };
 
-console.log(addBorder(['abc', 'ded']));
+//console.log(addBorder(['abc', 'ded']));
+
+
+// Two arrays are called similar if one can be obtained from
+// another by swapping at most one pair of elements in one
+// of the arrays.
+// Given two arrays a and b, check whether they are similar.
+function areSimilar(a, b) {
+
+};
+
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
